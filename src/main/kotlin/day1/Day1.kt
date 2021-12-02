8d90276655d93fac03af7db7bcd8500612d6e57b
Frode Reinertsen <frode.reinertsen@gmail.com>
2021-12-02 10:04:30 +0100

    day1 - with inspiration

--- src/main/kotlin/day1/Day1.kt
+++ src/main/kotlin/day1/Day1.kt
@@ -1,22 +1,4 @@
 package day1
 
-fun numIncreases(depths: List<Int>, windowSize: Int): Int {
-    var numIncreases = 0
-    val memo = mutableMapOf<Int, Int>()
-
-    fun sumOfWindow(index: Int): Int = memo.getOrPut(index) {
-        var sum = 0
-        for (i in index downTo (index - windowSize + 1)) {
-            sum += depths[i]
-        }
-        return sum
-    }
-
-    for (index in windowSize until depths.size) {
-        if (sumOfWindow(index) > sumOfWindow(index - 1)) {
-            numIncreases++
-        }
-    }
-
-    return numIncreases
-}
+fun numIncreases(depths: List<Int>, windowSize: Int): Int =
+    depths.windowed(windowSize).zipWithNext().count { it.second.sum() > it.first.sum() }
